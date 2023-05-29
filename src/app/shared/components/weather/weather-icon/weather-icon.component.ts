59dd7225db8e34ad3cf369d1a8b3e164458288e7
Neitsabes <sebcayet@gmail.com>
2023-05-29 08:06:31 +0200

    :recycle: Changes weather icons img tag to ion-img tag

--- src/app/shared/components/weather/weather-icon/weather-icon.component.ts
+++ src/app/shared/components/weather/weather-icon/weather-icon.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-weather-icon',
   templateUrl: './weather-icon.component.html',
   styleUrls: ['./weather-icon.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, IonicModule]
 })
 export class WeatherIconComponent  implements OnInit {
-  
+
   public readonly iconBaseUrl = 'http://openweathermap.org/img/wn/';
   public readonly iconExtension = '.png';
   @Input() public iconSize = 32;
