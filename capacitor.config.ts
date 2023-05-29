af92ff1493d464ccc862be039a680ca998550a09
Neitsabes <sebcayet@gmail.com>
2023-05-29 09:38:26 +0200

    :bento: Adds splash screens and icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,20 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Weather',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
